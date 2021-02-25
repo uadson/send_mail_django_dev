4ab2fc23521a31ea719d67283f11cb8d79d5e9ba
uadson <uadsoncastro@gmail.com>
2021-02-25 12:13:26 -0300

    added template home.html

--- sendmail/settings.py
+++ sendmail/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'sendmail.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'sendmail/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_HOST_USER = 'postmaster@sandboxcb9af1a328264e12893b899e6a760eb1.mailgun.org'
+EMAIL_HOST_PASSWORD = '79649b8369b82312cfbf76aafb1a192a-6e0fd3a4-8513faaf'
+EMAIL_PORT = 587
--- userform/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserformConfig(AppConfig):
-    name = 'userform'
